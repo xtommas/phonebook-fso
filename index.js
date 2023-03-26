b7ba1581fed4cb6b854b23547d9cbfe90c5494be
tomas <tomascanepa1397@gmail.com>
2023-03-26 12:58:52 -0300

    added MongoDB database

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Contact = require('./models/contact')
 
 
 const app = express()
 
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan('tiny'))
 // middleware for deploying
 app.use(cors())
-app.use(express.static('build'))
+
 
 let contacts = [
     { 
@@ -35,75 +38,101 @@ let contacts = [
 ]
 
 app.get('/info', (request, response) => {
-    const numberOfContacts = contacts.length
     const currentDate = new Date()
 
-    response.send('<p>Phonebook has info for ' + numberOfContacts + ' people</p><p>' + currentDate + '</p>')
+    Contact.find({}).then(contacts => {
+        response.send('<p>Phonebook has info for ' + contacts.length + ' people</p><p>' + currentDate + '</p>')
+    })
 })
 
 app.get('/api/contacts', (request, response) => {
-    response.json(contacts)
+    Contact.find({}).then(contacts => {
+        response.json(contacts)
+    })
 })
 
-app.get('/api/contacts/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const contact = contacts.find(contact => contact.id === id)
-
-    if(contact) {
-        response.json(contact)
-    } else{
-        response.status(404).end()
-    }
+app.get('/api/contacts/:id', (request, response, next) => {
+    Contact.findById(request.params.id).then(contact => {
+        if(contact) {
+            response.json(contact)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/contacts/:id', (request, response) => {
-    const id = Number(request.params.id)
-    contacts = contacts.filter(contact => contact.id !== id)
-
-    response.status(204).end()
+app.delete('/api/contacts/:id', (request, response, next) => {
+    Contact.findByIdAndRemove(request.params.id).then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-function generateId(){
-    const maxId = contacts.length > 0 ? Math.max(...contacts.map(c => c.id)) : 0
+app.put('/api/contacts/:id', (request, response, next) => {
+    const { name, number } = request.body
 
-    return maxId + 1
-}
+    Contact.findByIdAndUpdate(
+        request.params.id,
+        { name, number },
+        {new: true, runValidators: true, context: 'query'} 
+    )
+        .then(updatedContact => {
+        response.json(updatedContact)
+    })
+    .catch(error => next(error))
+})
+
+// function generateId(){
+//     const maxId = contacts.length > 0 ? Math.max(...contacts.map(c => c.id)) : 0
+
+//     return maxId + 1
+// }
 
 function nameAlreadyInContacts(name) {
-    for(let i = 0; i<contacts.length; i++) {
-        if(contacts[i].name === name) {
-            return true
+    Contact.find({}).then(contacts => {
+        for(let i = 0; i<contacts.length; i++) {
+            if(contacts[i].name === name) {
+                return true
+            }
         }
-    }
+    })
 
     return false
 }
 
-app.post('/api/contacts', (request, response) => {
+app.post('/api/contacts', (request, response, next) => {
     if(!request.body.name || !request.body.number) {
         return response.status(400).json({
             error: 'content missing'
         })
     }
 
-    if(nameAlreadyInContacts(request.body.name)) {
-        return response.status(400).json({
-            error: 'name must be unique'
-        })
-    }
-
-    const contact = {
-        id: generateId(),
+    const contact = new Contact({
         name: request.body.name,
         number: request.body.number,
-    }
+    })
 
-    contacts = contacts.concat(contact)
-
-    response.json(contact)
+    contact.save().then(savedContact => {
+        response.json(savedContact)
+    }).catch(error => next(error))
 })
 
-const PORT = process.env.PORT || 3001
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+    next(error)
+}
+  
+// this has to be the last loaded middleware.
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
